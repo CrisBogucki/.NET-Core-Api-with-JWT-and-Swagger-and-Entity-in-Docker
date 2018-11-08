7e72dfaa77954c9d8bd3e10203727a9c45d6e2a4
Krzysztof Bogucki <krzysztof.lubien@icloud.com>
2018-11-08 21:30:30 +0100

    Add using token

--- Events/Query/Account/LoginQueryHandler.cs
+++ Events/Query/Account/LoginQueryHandler.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using MediatR;
+using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.Tokens;
 using WebApi.Entities;
 using WebApi.Events.Command.Account;
@@ -16,12 +17,13 @@ namespace WebApi.Events.Query.Account
 {
     public class LoginQueryHandler : INotification, IRequestHandler<LoginQuery, LoginRequest>
     {
-        //private readonly AppSettings _appSettings;
+        private readonly AppSettings _appSettings;
         private readonly IUserRepository _userRepository;
 
-        public LoginQueryHandler(IUserRepository userRepository)
+        public LoginQueryHandler(IUserRepository userRepository, IOptions<AppSettings> appSettings)
         {
             _userRepository = userRepository;
+            _appSettings = appSettings.Value;
         }
 
         public async Task<LoginRequest> Handle(LoginQuery query, CancellationToken cancellationToken)
@@ -31,22 +33,21 @@ namespace WebApi.Events.Query.Account
                 return null;
 
             LoginRequest result = new LoginRequest {FirstName = user.FirstName, LastName = user.LastName};
-//
-//            JwtSecurityTokenHandler tokenHandler = new JwtSecurityTokenHandler();
-//            byte[] key = Encoding.ASCII.GetBytes(_appSettings.Secret);
-//            SecurityTokenDescriptor tokenDescriptor = new SecurityTokenDescriptor
-//            {
-//                Subject = new ClaimsIdentity(new Claim[]
-//                {
-//                    new Claim(ClaimTypes.Name, user.Id.ToString())
-//                }),
-//                Expires = DateTime.UtcNow.AddMinutes(_appSettings.SessionTime),
-//                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key),
-//                    SecurityAlgorithms.HmacSha256Signature)
-//            };
-//            
-//            SecurityToken token = tokenHandler.CreateToken(tokenDescriptor);  
-//            result.Token = tokenHandler.WriteToken(token);
+            JwtSecurityTokenHandler tokenHandler = new JwtSecurityTokenHandler();
+            byte[] key = Encoding.ASCII.GetBytes(_appSettings.Secret);
+            SecurityTokenDescriptor tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[]
+                {
+                    new Claim(ClaimTypes.Name, user.Id.ToString())
+                }),
+                Expires = DateTime.UtcNow.AddMinutes(_appSettings.SessionTime),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key),
+                    SecurityAlgorithms.HmacSha256Signature)
+            };
+            
+            SecurityToken token = tokenHandler.CreateToken(tokenDescriptor);  
+            result.Token = tokenHandler.WriteToken(token);
             
             return result;
         }
